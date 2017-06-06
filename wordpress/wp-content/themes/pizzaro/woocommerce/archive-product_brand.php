60a2ced4a1fceb8bc2efc74efb6faf2a90c5fa48
Toy Mach1ne <forvinwolves@gmail.com>
2017-06-06 14:41:14 +0300

    Fp, brands page

--- wordpress/wp-content/themes/pizzaro/woocommerce/archive-product_brand.php
+++ wordpress/wp-content/themes/pizzaro/woocommerce/archive-product_brand.php
@@ -26,24 +26,28 @@ get_header( 'v1' ); ?>
         <?php if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
           foreach ( $terms as $term ) { $termlinks= get_term_link($term,$taxonomy); ?>
            <div class="col-md-6">
-            <a href="<?php echo $termlinks ?>">
              <div class="brand_inner">
                <div class="brand_img_wrapp">
+                 <a href="<?php echo $termlinks ?>">
                     <?php $thumbnail_id = get_woocommerce_term_meta($term->term_id, 'thumbnail_id', true);
                     $image = wp_get_attachment_url($thumbnail_id); ?>
                     <?php echo "<img src='{$image}' alt='' width='400' height='400' />"; ?>
+                 </a>
                </div>
                <div class="brand_content_wrapp">
                  <h4 class="brand_title">
-                  <?php echo $term->name; ?>
+                  <a href="<?php echo $termlinks ?>"><?php echo $term->name; ?></a>
                  </h4>
                  <?php echo term_description($term->term_id, 'product_brand'); ?>
+                 <?php $kitchens = get_field('kitchens', $term); ?>
                  <div class="kitchen_show">
-
+                   <div class="card__short-info summary">
+                     Кухни: <?php foreach ($kitchens as $kitchen) { $termlinks= get_term_link($kitchen,$taxonomy); ?>
+                     <a href="<?php echo $termlinks; ?>"> <?php echo $kitchen->name . " "; ?> </a> <?php }?>
+                   </div>
                  </div>
                </div>
              </div>
-            </a>
            </div>
         <?php }} ?>
       </div>
